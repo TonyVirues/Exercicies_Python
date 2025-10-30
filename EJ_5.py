3abfd403b7b822ea3944af7f77c16f4267698463
TonyVirues <tonimavir@gmail.com>
2025-10-30 21:04:30 +0100

    add_ej_5_6

---
+++ EJ_5.py
@@ -0,0 +1,5 @@
+long_radio=float(input("Escriba la longitud del radio "))
+diametro=long_radio*2
+long_circunsferencia=3.14*diametro
+area_circulo=3.14**2
+print("El área del circulo es ")
---
+++ UD03_EJ_UD03/EJ_4.py
@@ -0,0 +1,8 @@
+from math import pi
+n_1=float(input("Escriba el primer numero "))
+n_2=float(input("Escriba el segundo numero "))
+sumar=n_1+n_2
+resta=n_1-n_2
+producto=n_1*n_2
+division=n_1/n_2
+print(f"La suma es {sumar}la resta es {resta}La multiplicacion es {producto}La division es {division}")
