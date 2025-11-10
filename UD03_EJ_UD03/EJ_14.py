8236546f1664a5b3bf749ca9172c5e83ebe7672c
TonyVirues <tonimavir@gmail.com>
2025-11-10 09:58:35 +0100

    added 14,15

---
+++ UD03_EJ_UD03/EJ_14.py
@@ -0,0 +1,8 @@
+n_1 = float(input("Escriba un primer número :"))
+n_2 = float(input("Escriba un segundo número :"))
+if n_1==n_2:
+    print("Los números son iguales")
+elif n_1>n_2:
+    print("El primer número es mayor")
+else:
+    print("El segundo número es mayor")
---
+++ UD03_EJ_UD03/EJ_15.py
@@ -0,0 +1,6 @@
+n_1=float(input("Escriba el primer número :"))
+n_2=float(input("Escriba el segundo número :"))
+n_3=float(input("Escriba el tercer número :"))
+if n_1>n_2:
+    if n_2>n_3:
+        print("El primer número es el mayor :")
