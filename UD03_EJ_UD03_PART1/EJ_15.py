fa4191e584ab3523780966c15b47ae743bc2ee7f
Tonyvirues <tonimavir@gmail.com>
2025-11-19 18:04:05 +0100

    add 15 and foro

--- UD03_EJ_UD03_PART1/EJ_15.py
+++ UD03_EJ_UD03_PART1/EJ_15.py
@@ -2,5 +2,43 @@ n_1=float(input("Escriba el primer número :"))
 n_2=float(input("Escriba el segundo número :"))
 n_3=float(input("Escriba el tercer número :"))
 if n_1>n_2:
-    if n_2>n_3:
-        print("El primer número es el mayor :")
+    if n_1==n_2:
+        if n_2>n_3:
+            print(f"{n_1} y {n_2} Son iguales")
+            print(f"{n_3}Es mas pequeño")
+        else:
+            print(f"{n_3}Es el mayor")
+            print(f"{n_1} y {n_2} Son iguales")
+    elif n_2>n_3:
+        print(f"{n_1}Es el mayor")
+        print(f"{n_2}Es el mediano")
+        print(f"{n_3}Es el pequeño")
+    else:
+        print(f"{n_1}Es el mayor")
+        print(f"{n_3}Es el mediano")
+        print(f"{n_2}Es el pequeño")
+elif n_2>n_3:
+    if n_3>n_1:
+        print(f"{n_2}Es el mayor")
+        print(f"{n_3}Es el mediano")
+        print(f"{n_1}Es el pequeño")
+    else:
+        print(f"{n_2}Es el mayor")
+        print(f"{n_1}Es el mediano")
+        print(f"{n_3}Es el pequeño")
+elif n_3>n_1:
+    if n_1>n_2:
+        print(f"{n_3}Es el mayor")
+        print(f"{n_1}Es el mediano")
+        print(f"{n_2}Es el pequeño")
+    else:
+        print(f"{n_3}Es el mayor")
+        print(f"{n_2}Es el mediano")
+        print(f"{n_1}Es el pequeño")
+elif n_1==n_2:
+    if n_2==n_3:
+        print("Todos los números son iguales")
+    else:
+        print("Uno y dos son iguales pero 3 no")
+else:
+    print("sigue probando")
--- UD03_FORO/EJ_17_11.py
+++ UD03_FORO/EJ_17_11.py
@@ -1,15 +1,7 @@
-
-
 #Ejercicio del foro
+#Primera figura
 altura=int(input("Escribe un número"))
-
-
-for i in range(1,altura+1):
-    for j in range(i,altura+1):
-        print("4",end="")
-    print("*")
-    
-print("7")
+print("4")
 for i in range(1,altura-1):
     print("4",end="")
     print(" "*(i-1),end="")
