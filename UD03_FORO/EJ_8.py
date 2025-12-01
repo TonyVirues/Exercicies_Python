07537ebcc725ce20bb6dd1e097fb5d3ca4701f97
Tonyvirues <tonimavir@gmail.com>
2025-12-01 19:17:26 +0100

    new banch and add ej_texto

--- UD03_FORO/EJ_8.py
+++ UD03_FORO/EJ_8.py
@@ -1,6 +1,7 @@
 #Ejercicio 8: Rombo sólido
 #Imprime un rombo sólido de altura 2n-1,centrado usando asteriscos. figura para n=4
-altura = 5
+n = int(input("Escribame su numerito :"))
+altura = 2*n-1
 for i in range(1,altura+1):
      print(" "*(altura-i)+"*"*(2*i-1))
 for i in range(altura,0,-1):
