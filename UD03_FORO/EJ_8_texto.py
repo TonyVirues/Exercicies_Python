acbe2cb8805d2ddb80ca0f494e296b36ff559183
TonyVirues <tonimavir@gmail.com>
2025-12-02 10:19:46 +0100

    add

--- UD03_FORO/EJ_8_texto.py
+++ UD03_FORO/EJ_8_texto.py
@@ -1,2 +1,8 @@
 #Convertir todas las letras a mayúsculas o minúsculas usando ciclos y sumas de caracteres (sin usar los métodos upper() o lower()).
 
+cadenaMayus = "caracola"
+cadenaLower = ""
+
+for i in range (len(cadenaMayus)):
+    cadenaMayus += chr(ord(cadenaLower[i])-32)
+print(cadenaMayus)
