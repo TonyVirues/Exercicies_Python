cbbef2ccec40349e828b1087e576347851527389
TonyVirues <tonimavir@gmail.com>
2025-12-02 09:33:17 +0100

    add changes

--- UD03_FORO/EJ_5_texto.py
+++ UD03_FORO/EJ_5_texto.py
@@ -1,10 +1,15 @@
 #verificar si un carácter especifico esta en la cadena con un ciclo y comparaciones.
 cadena = input("Digame la palabra: ")
-letra = input("Que caracter quieres comprobar si se encuentra")
+letra = input("Que caracter quieres comprobar si se encuentra: ")
 contador = 0
+bandera = False
 for i in cadena:
     if letra==i:
+        bandera = True
         contador+=1
     else:
-        print()
-print(f"La letra selecionada aparecio un total de {contador} veces ")
+        print("")
+if bandera==True:
+    print(f"La letra selecionada aparecio. Un total de {contador} veces.")
+else:
+    print("La letra introducida nose encuentra en la cadena.")
